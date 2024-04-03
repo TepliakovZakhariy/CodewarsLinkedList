9d4d6cf7e63ab861cbd381d747be5bd30901ba8b
cheeese <tepliakov.pn@ucu.edu.ua>
2024-04-03 14:15:06 +0300

    Completed "Convert a linked list to a string"

---
+++ Convert a linked list to a string/problem.py
@@ -0,0 +1,8 @@
+def stringify(node):
+    result=''
+    current=node
+    while current:
+        result+=str(current.data)+' -> '
+        current=current.next
+    result+='None'
+    return result
